9e33ad43f254ce88dea16f41784c897fa6233123
dragonist <erin314@naver.com>
2015-09-21 17:11:07 +0900

    #3 sesstion-storage 보다 localstorage가 적합하여 localforage 적용함

--- alert-page.js
+++ alert-page.js
@@ -13,7 +13,9 @@
 		}
 		Alert.activeOne = $(this.Container);
 		Alert.activeOne.addClass("active");
-		e.stopPropagation();
+		if(e){
+			e.stopPropagation();
+		}
 	};
 
 	Alert.prototype.setAnimation = function(type) {
@@ -23,10 +25,44 @@
 		$(this.Container).addClass("animation");
 		$(this.Container).addClass("animation-"+type);
 	};
+	Alert.prototype.setSession = function(id) {
+		var key = 'alert-page'+id;
+		$(this.Container).data("key", key);
+		localforage.getItem(key, function (err, result) {
+			if(result){
+				if(formDate(new Date()) === result.due && !result.show){
+					var input = $(this.Container).find("input[type=checkbox]")[0];
+					input.checked = true;
+				}else{
+					this.showAlert();
+				}
+			}else{
+				result = {show:true, due:formDate(new Date())};
+				localforage.setItem(key, result);
+				this.showAlert();
+			}
+		}.bind(this));
+	};
 
+	function formDate (date) {
+		var yyyy = date.getFullYear().toString();
+		var mm = (date.getMonth()+1).toString();
+		var dd  = date.getDate().toString();
+		return yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]);
+	}
 
 	function close(e){
 		if(Alert.activeOne){
+			var checkbox = Alert.activeOne.find("input[type=checkbox]")[0];
+			if(checkbox){
+				var key = Alert.activeOne.data("key");
+				localforage.getItem(key, function (err, result) {
+					if(result.show === checkbox.checked){
+						result.show = !checkbox.checked;
+						localforage.setItem(key, result);
+					}
+				})
+			}
 			Alert.activeOne.removeClass("active");
 			Alert.activeOne = null;
 		}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function () {
-	new Alert("#hello1")
+	new Alert("#hello1").setSession(1234);
 	new Alert("#hello2").setAnimation("opacity");
 	new Alert("#hello3").setAnimation("opacity");
 });
